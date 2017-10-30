182aafdb1ce41d71a0758fbc17ccb886c073dfa2
Giuseppe Mascolo <giuseppe.mascolouk@gmail.com>
2017-10-30 16:03:20 +0000

    Added main
    At the moment mainPortfolioOptimiser.m just read data from the database and clean them.

---
+++ mainPortofolioOptimiser.m
@@ -0,0 +1,26 @@
+%--------------------------------------------------------------------------
+% Portfolio Optimiser main script
+%--------------------------------------------------------------------------
+
+%Reads data from database
+Market = readtable('DB/Market_Data.csv');
+Compound = readtable('DB/Market_Data_CR.csv');
+Return = readtable('DB/Market_Data_NR.csv');
+
+%cleans data from NaN values
+R = height(Market); %gives the number of rows
+Compound(R,:) = []; %Last day does not have any return value 
+Return(R,:)   = []; %and therefore deleted
+Dcolumns = find( sum(ismissing(Compound)) > 0 ); %Find columns that have NaN values
+Compound(:,Dcolumns) = []; %deletes columns with NaN values
+Market(:,Dcolumns) = [];   % "                           "
+Return(:,Dcolumns) = [];   % "                           "
+
+%https://uk.mathworks.com/help/matlab/matlab_prog/access-data-in-a-table.html
+%to learn how to access data 
+
+Companies = Market.Properties.VariableNames; %creates a vector of cells with companies tickers
+size = length(Companies); %counts the number of columns left
+
+
+
