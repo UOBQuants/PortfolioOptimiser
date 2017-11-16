530c6f272e5398853fb54f3b720de3d115d9914f
GiuseppeMascolo <32720158+GiuseppeMascolo@users.noreply.github.com>
2017-11-16 11:33:21 +0000

    Moved Fat_Tails
    made also same little changes

---
+++ functions/Fat_Tails.m
@@ -0,0 +1,27 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%% Identification Fat tail %%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% To find out the fat tails, we compare the data with a fat tailed
+% distribution (here the student t)
+% Df (degree of freedom) indicate how fat the tails are
+
+function Fat_Tails (Return,size,Companies)
+
+prob_cdf = 0.005:0.01:0.995; % confidence interval
+quantile_t_dist = tinv (prob_cdf, 3); 
+%return the inverse of the student (=quantile function)
+%3 are the degree of freedom of the tStudent distribution considered
+%we choose 3 after having tried some, it can be changed.
+
+% Plot T distribution VS Outcome company 
+for i = 3:1:size
+    simplereturn = Return{ : , i:size};
+    
+    subplot( 2, 2, i-2)
+    qqplot( simplereturn, quantile_t_dist );
+    name = char(Companies(i);
+    title( strcat(name) , 'T Distribution Quantiles (df=3)') );
+end
+
+end
