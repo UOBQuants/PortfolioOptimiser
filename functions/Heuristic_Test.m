19835d5e8fb12698e9c5e9ba32df9249db8dc6ea
Ashley Robertson <me@ashpace.co.uk>
2017-11-20 12:34:01 +0000

    Draft1 - ready for pull

--- functions/Heuristic_Test.m
+++ functions/Heuristic_Test.m
@@ -7,9 +7,15 @@ function output = Heuristic_Test(Data, CompanyName, Visual)
 % under i.i.d. the location-dispersion ellipsoid should be a circle
 % see "Risk and Asset Allocation"-Springer (2005), by A. Meucci
 
+%% Adding sub-functions
+addpath('Heuristic_test_sub_functions')
+
+%% Setting final results to false. If output is true, the Data is IID
 mean_result = false;
 sd_result = false;
+correlation_signal = false;
 
+%% Dividing data into two halfs and comoparing the halfs
 % test "identically distributed hypothesis": split observations into two sub-samples and plot histogram
 sample_size = (length(Data)/2);
 Sample_1=Data(1:round(sample_size));
@@ -22,29 +28,47 @@ pd2 = fitdist(Sample_2,'Normal');
 
 
 mean1 = pd1.mu; %mean of sample1
-sd1 = sqrt(pd1.sigma); %Standard deviation of sample2
-Sigma_mean1 = sd1/sqrt(sample_size); %Standard error of the mean
+sd1 = pd1.sigma; %Standard deviation of sample 1
 
-Z_95 = tinv(1-0.05,sample_size);
+
+Sigma_mean1 = sd1/sqrt(sample_size); %Standard error of the mean
+Z_95 = tinv(1-0.025,sample_size); %t critical value
 
 mean2 = pd2.mu; %mean of sample2
+sd2 = pd2.sigma;%Standard deviation of sample 2
 
-Z_95 = 1.96; %Defining 95% confidence interval
+Upper_Chi2_95 = chi2inv(1-0.975,(sample_size-1));
+Lower_Chi2_95 = chi2inv(0.975,(sample_size-1));
 
+Mean_Lower_limit = mean1 - Z_95*Sigma_mean1;
+Mean_Upper_limit = mean1 + Z_95*Sigma_mean1;
+%Confidence interval calculation comes from http://www.milefoot.com/math/stat/ci-means.htm
 
-Lower_limit = mean1 - Z_95*Sigma_mean1;
-Upper_limit = mean1 + Z_95*Sigma_mean1;
-if Lower_limit < mean2 && Upper_limit > mean2 %if the mean of sample 2 is 
+Sigma_Upper_limit = sqrt(((sample_size-1)*sd1^2)/Upper_Chi2_95);
+Sigma_Lower_limit = sqrt(((sample_size-1)*sd1^2)/Lower_Chi2_95);
+
+%Confidence interval calculation comes from http://www.milefoot.com/math/stat/ci-variances.htm
+
+if Mean_Lower_limit < mean2 && Mean_Upper_limit > mean2 %if the mean of sample 2 is 
     %within the range of mean of sample1 +- sd1
     mean_result = true;
+end
+
+if Sigma_Lower_limit < sd2 && Sigma_Upper_limit > sd2 %if the mean of sample 2 is 
+    %within the range of mean of sample1 +- sd1
+    sd_result = true;
 end
 
 D = Data(1:end-1);
 L = Data(2:end);
 %Define the lags
 
-m=mean([D , L]);
-S=cov([D , L]);
+Correlation = corr(D,L);
+p = compare_correlation_coefficients(Correlation,0,length(Data),100000000);
+%The probability that actual correlation is zero. 
+if p > 0.2
+    correlation_signal = true;
+end
 
 if Visual == true 
     num_bins_1 = round(5*log(length(Sample_1)));
@@ -77,10 +101,11 @@ if Visual == true
     title(Hist1_Title)
     xlabel('Price')
     ylabel('Frequncy')
+    height = max(n2)/100;
     txt1 = ['Mean = ', num2str(pd1.mu)];
-    text(X_lim(2)/2,10,txt1)
+    text(X_lim(2)/2,height*40,txt1)
     txt2 = ['Sigma^2 = ', num2str(pd1.sigma)];
-    text(X_lim(2)/2,8,txt2)
+    text(X_lim(2)/2,height*30,txt2)
     %Adding information to the plot
 
     subplot('Position',[.53 .52 .44 .42])
@@ -92,12 +117,13 @@ if Visual == true
     title(Hist2_Title)
     xlabel('Price')
     ylabel('Frequncy')
+    height = max(n2)/100;
     txt1 = ['Mean = ', num2str(pd2.mu)];
-    text(X_lim(2)/2,10,txt1)
+    text(X_lim(2)/2,height*40,txt1)
     txt2 = ['Sigma^2 = ', num2str(pd2.sigma)];
-    text(X_lim(2)/2,8,txt2)
+    text(X_lim(2)/2,height*30,txt2)
     %adding information to the plot
-
+    
     % test "independently distributed hypothesis": scatter plot of observations at lagged times
     subplot('Position',[.28 .01 .43 .43])
     plot( D , L , '.' );
@@ -108,5 +134,5 @@ if Visual == true
     set(gca,'xlim',X_lim,'ylim',X_lim);
 end
 
-output = [mean_result, sd_result];
+output = [mean_result, sd_result, correlation_signal];
 end
--- functions/backtest/Heuristic_IID_Backtest.m
+++ functions/backtest/Heuristic_IID_Backtest.m
@@ -6,7 +6,7 @@
 mu = 0;
 sigma = 0.7;
 n = 365; %Simulating one year data
-C = 0.5; %Coefficient of dependence to the previous day
+C = 1; %Coefficient of dependence to the previous day
 
 X = normrnd(mu,sigma,n,1); % n number of IID random number
 pX = [0 ; X(1:end-1)].*C; %Pertubation vector
@@ -14,5 +14,6 @@ NX = X + pX;     %Creating Non IID X as NX.
 
 %%
 addpath('../')
+addpath('../Heuristic_test_sub_functions')
 Heuristic_Test(X, 'IID invariant', true)
 Heuristic_Test(NX, 'Non-IID invariant', true)
