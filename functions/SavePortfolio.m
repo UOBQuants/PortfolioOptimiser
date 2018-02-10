67d469e99fb5842125e8e28b0d98286cd8fde484
Ashley Robertson <me@ashpace.co.uk>
2018-02-10 15:24:20 +0000

    SavePortfolio_BL
    Co-Authored-By: Giuseppe Mascolo <giuseppemascolo@users.noreply.github.com>

--- functions/SavePortfolio.m
+++ functions/SavePortfolio.m
@@ -1,18 +1,36 @@
-function SavePortfolio(companies, Weights, Date)
+function SavePortfolio(companies, Weights, Date, Method)
+
+
 size = length(companies);
 Portfolio(1) = Date;
 Portfolio(2:size+1) = Weights;
 
-if exist('PortfolioDB.mat', 'file') == 0 
-    Vars(2:size+1) = companies;
-    Vars(1) = {'Date'};
-    
-    PortfolioDB = table;
-    PortfolioDB = [PortfolioDB, Portfolio];
-    PortfolioDB.Properties.VariableNames = Vars;
-    save PortfolioDB
-else
-    load PortfolioDB
-    PortfolioDB = [PortfolioDB; Portfolio];
-    save PortfolioDB
+if Method == 'NP'
+    if exist('PortfolioDB.mat', 'file') == 0 
+        Vars(2:size+1) = companies;
+        Vars(1) = {'Date'};
+
+        PortfolioDB = table;
+        PortfolioDB = [PortfolioDB, Portfolio];
+        PortfolioDB.Properties.VariableNames = Vars;
+        save PortfolioDB
+    else
+        load PortfolioDB
+        PortfolioDB = [PortfolioDB; Portfolio];
+        save PortfolioDB
+    end
+elseif Method == 'BL'
+    if exist('PortfolioDB_BL.mat', 'file') == 0 
+        Vars(2:size+1) = companies;
+        Vars(1) = {'Date'};
+
+        PortfolioDB_BL = table;
+        PortfolioDB_BL = [PortfolioDB_BL, Portfolio];
+        PortfolioDB_BL.Properties.VariableNames = Vars;
+        save PortfolioDB_BL
+    else
+        load PortfolioDB_BL
+        PortfolioDB_BL = [PortfolioDB_BL; Portfolio];
+        save PortfolioDB_BL
+    end
 end
